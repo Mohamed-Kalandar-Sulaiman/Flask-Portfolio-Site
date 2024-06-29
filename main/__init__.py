2546e261594689ac80b69251943890ae05d70b81
Mohamed K Sulaiman <sulaimanhamsa555@gmail.com>
2024-06-29 16:51:12 +0530

    added other sections

--- main/__init__.py
+++ main/__init__.py
@@ -6,3 +6,4 @@ def create_flask_app():
     app = Flask(__name__)
     app.register_blueprint(ui_blueprint, url_prefix = "/")    
     return app
+
--- main/ui/view.py
+++ main/ui/view.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
-
+from flask import request
 ui_blueprint = Blueprint("ui", __name__, template_folder="templates", static_folder="static", static_url_path="/ui")
 
 @ui_blueprint.route("/", methods=['GET'])
 def home():
+
+    client_ip = request.remote_addr
+    user_agent = request.headers.get('User-Agent')
+    print("IP ", client_ip)    
     return render_template("home.html")
+
+

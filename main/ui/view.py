fcad5e8f7831b7d3d06b9580defc070c4ea7bd04
Mohamed K Sulaiman <sulaimanhamsa555@gmail.com>
2024-07-07 16:03:06 +0530

    Uploaded into pythonanyehere

--- main/ui/view.py
+++ main/ui/view.py
@@ -7,12 +7,18 @@ ui_blueprint = Blueprint( "ui",
                           static_url_path="/ui"
                           )
 
+
+
 @ui_blueprint.route("/", methods=['GET'])
 def home():
+    queryParams = request.args
+    whoIsIt = queryParams.get("me",None)
+    if whoIsIt== None :
+        client_ip = request.remote_addr
+        user_agent = request.headers.get('User-Agent')
+        print("IP ", client_ip)    
+        print("User Agent ", user_agent)    
 
-    client_ip = request.remote_addr
-    user_agent = request.headers.get('User-Agent')
-    print("IP ", client_ip)    
     return render_template("home.html")
 
 
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from main import create_flask_app
 
+app = create_flask_app()
 
 if __name__ =="__main__":
-    app = create_flask_app()
     app.run(debug=True)
 
 
